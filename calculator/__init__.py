5b7bfbb289c4311d3110be1654952f5267a15ec6
varshith-29 <93570505+varshith-29@users.noreply.github.com>
2025-02-16 17:22:53 -0500

    Calculator and operations

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,18 @@
-def add(a,b):
-    return a + b
+from calculator.operations import addition, subtraction, multiplication, division
+from decimal import Decimal 
 
-def subtract(a,b):
-    return a - b
+class Calculator:
+    def __init__(self, history):
+        self.history = history
+
+    def add(a, b):
+        return addition(a, b)
+
+    def subtract(a: Decimal, b: Decimal) -> Decimal:
+        return subtraction(a, b)
+
+    def multiply(a: Decimal, b: Decimal) -> Decimal:
+        return multiplication(a, b)
+
+    def divide(a: Decimal, b: Decimal) -> Decimal:
+        return division(a, b)
